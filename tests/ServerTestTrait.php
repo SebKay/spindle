3de01410e89d3ee8e4f0a9eb0a32a3364f104f77
Seb Kay <seb@sebkay.com>
2020-11-19 22:09:23 +0000

    Added test for ExampleMiddleware

---
+++ tests/ServerTestTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Factory\ServerRequestFactory;
+
+trait ServerTestTrait
+{
+    /**
+     * Create a quick server request
+     *
+     * @param string $method
+     * @param string $uri
+     * @param array $serverParams
+     * @return ServerRequestInterface
+     */
+    protected function createRequest(string $method, string $uri, array $serverParams = []): ServerRequestInterface
+    {
+        return (new ServerRequestFactory())->createServerRequest($method, $uri, $serverParams);
+    }
+}
---
+++ tests/Unit/Middleware/ExampleMiddlewareTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Middleware;
+
+use App\Middleware\ExampleMiddleware;
+use Slim\Factory\AppFactory;
+use Tests\ServerTestTrait;
+use Tests\Unit\Test;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class ExampleMiddlewareTest extends Test
+{
+    use ServerTestTrait;
+
+    public function setUp(): void
+    {
+        //
+    }
+
+    /**
+     * @testdox It returns a valid response when called
+     */
+    public function test_it_returns_a_response()
+    {
+        $middleware = new ExampleMiddleware();
+        $request    = $this->createRequest('GET', '/');
+        $hander     = AppFactory::create();
+
+        $hander->get('/', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+            return $response;
+        });
+
+        $this->assertInstanceOf('\Psr\Http\Message\ResponseInterface', $middleware($request, $hander));
+    }
+}

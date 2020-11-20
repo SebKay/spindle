67389ec9eaa267b3574333dc8d513a0831ae2c5a
Seb Kay <seb@sebkay.com>
2020-11-20 13:16:24 +0000

    Fixed code coverage being less then 100% (also fixed possibly null value)

---
+++ tests/Unit/Dependencies/ViewTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit\Middleware;
+
+use App\Dependencies\View;
+use Tests\Unit\Test;
+
+class ViewTest extends Test
+{
+    /**
+     * @testdox It returns a string when rendering
+     */
+    public function test_it_renders_a_string()
+    {
+        $view = new View(
+            __DIR__ . '/../../../resources/views',
+            ''
+        );
+
+        $this->assertIsString($view->render('home.twig'));
+    }
+}

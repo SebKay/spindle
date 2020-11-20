698b30f7329e9483c02aa39763d5cbeb9e21ab4c
Seb Kay <seb@sebkay.com>
2020-11-20 12:58:20 +0000

    Removed var_dump

--- inc/routes/web.php
+++ inc/routes/web.php
@@ -4,8 +4,6 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 $this->slim->get('/', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
-    var_dump($this->has('View'));
-
     $response->getBody()->write($this->get('View')->render('home.twig', [
         'name' => 'Jim'
     ]));

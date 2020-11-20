acbae4255da493c442677e0f6deb4216ee73f9f9
Seb Kay <seb@sebkay.com>
2020-11-20 13:07:30 +0000

    Created controller for home

--- inc/routes/web.php
+++ inc/routes/web.php
@@ -1,12 +1,5 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use App\Controllers\HomeController;
 
-$this->slim->get('/', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
-    $response->getBody()->write($this->get('View')->render('home.twig', [
-        'name' => 'Jim'
-    ]));
-
-    return $response;
-});
+$this->slim->get('/', HomeController::class . ':index');

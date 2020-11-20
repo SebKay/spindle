27437b524ddfd70d459cee02420c6f94e0c1b9f3
Seb Kay <seb@sebkay.com>
2020-11-20 13:18:00 +0000

    Now using getenv instead of $_ENV as it could possibly be null

--- src/App.php
+++ src/App.php
@@ -39,7 +39,7 @@ class App
      */
     public function __construct()
     {
-        $this->dev_mode = (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] == 'development' ? true : false);
+        $this->dev_mode = (getenv('APP_ENV') == 'development' ? true : false);
 
         $this->container = new Container();
 

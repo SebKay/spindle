7862ef51c76444b266a30af90885b1d35b5ced4d
Seb Kay <seb@sebkay.com>
2021-02-11 21:23:29 +0000

    Add Sass support to Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,43 @@
 let webpack = require('webpack');
-let path = require('path')
+let path = require('path');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './resources/assets/js/app.js',
+module.exports = [
+    {
+        entry: [
+            './resources/assets/js/app.js',
+            './resources/assets/scss/app.scss',
+        ],
 
-    output: {
-        path: path.resolve(__dirname, 'public/assets/js'),
-        filename: 'app.js',
-        publicPath: './public',
+        output: {
+            path: path.resolve(__dirname, 'public/assets/js'),
+            filename: 'app.js',
+            publicPath: './public',
+        },
+
+        resolve: {
+            alias: {
+                'vue': 'vue/dist/vue.esm-bundler.js'
+            }
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                }
+            ],
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '../css/app.css',
+            }),
+        ],
     },
-
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.esm-bundler.js'
-        }
-    },
-};
+];

fb9b6afa51fb24caba1a76b2a3214a071aba40a6
Seb Kay <seb@sebkay.com>
2021-03-02 20:10:11 +0000

    Group routes as a better example

--- inc/routes/web.php
+++ inc/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Controllers\HomeController;
+use Slim\Routing\RouteCollectorProxy;
 
-$this->slim->get('/', HomeController::class . ':index');
-$this->slim->post('/', HomeController::class . ':update');
+/**
+ * Home
+ */
+$this->slim->group('', function (RouteCollectorProxy $group) {
+    $group->get('/', HomeController::class . ':index')->setName('home');
+    $group->post('/', HomeController::class . ':update');
+});

6a9f9eff70207f6d67b8ee8f7ad7c3b4f78ceef6
Seb Kay <seb@sebkay.com>
2021-03-09 20:25:05 +0000

    Add Webpack notifier plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 let webpack = require('webpack');
 let path = require('path');
 let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = (env, options) => {
     return {
+        stats: 'minimal',
+
         entry: [
             './resources/assets/js/app.js',
             './resources/assets/scss/app.scss',
@@ -37,6 +40,13 @@ module.exports = (env, options) => {
         plugins: [
             new MiniCssExtractPlugin({
                 filename: '../css/app.css',
+            }),
+            new WebpackNotifierPlugin({
+                emoji: true,
+                alwaysNotify: true,
+                title: function (params) {
+                    return `Slim Starter`;
+                }
             }),
         ],
     }

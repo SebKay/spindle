3cfa0027325004dcdf76a3695036fc6599c91aad
Seb Kay <seb@sebkay.com>
2021-05-13 21:32:50 +0100

    Add fade transition to modal

--- resources/assets/js/src/components/Modal.vue
+++ resources/assets/js/src/components/Modal.vue
@@ -1,25 +1,27 @@
 <template>
-    <div :id="id" class="modal">
-        <div class="modal__outer">
-            <div class="modal__inner">
-                <div class="modal__header">
-                    <h6 class="modal__title">
-                        <slot name="title"></slot>
-                    </h6>
-                </div>
+    <transition name="fade">
+        <div :id="id" class="modal">
+            <div class="modal__outer">
+                <div class="modal__inner">
+                    <div class="modal__header">
+                        <h6 class="modal__title">
+                            <slot name="title"></slot>
+                        </h6>
+                    </div>
 
-                <div class="modal__content">
-                    <slot></slot>
-                </div>
+                    <div class="modal__content">
+                        <slot></slot>
+                    </div>
 
-                <div class="modal__footer">
-                    <button class="btn" @click="close()">
-                        Close
-                    </button>
+                    <div class="modal__footer">
+                        <button class="btn" @click="close()">
+                            Close
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </transition>
 </template>
 
 <script>
